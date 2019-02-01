d65f2577a6121e22ee398fb36a217cdb409e7ebc
EtgarZ <etgarzyl92@gmail.com>
2019-02-01 11:49:54 +0200

    add routing for flights

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FlightListComponent } from "./flights/flight-list/flight-list.component";
+import { FlightCreateComponent } from "./flights/flight-create/flight-create.component";
+
+const routes: Routes = [
+  { path: '', component: FlightListComponent },
+  { path: 'create', component: FlightCreateComponent },
+  { path: 'edit/:flightId', component: FlightCreateComponent },
+  { path: 'read', component: FlightListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
